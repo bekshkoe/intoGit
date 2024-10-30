fbcd0b0696b655fd84819f0ccc29aa9c9fb3f632
Beksultan <beka47674@gmail.com>
2024-10-31 01:59:44 +0500

    HomeWork

--- Jscript.js
+++ Jscript.js
@@ -1,1 +1,26 @@
-console.log("Beksultan2");
+let name = "Beksultan"
+
+console.log("My name:" +name+ "/ Type: " + typeof name );
+
+const yerth = 17;
+
+console.log("My yerth:" + yerth + "/ Type: " + typeof yerth);
+
+let score = false
+if (true) {
+   let score = true
+   console.log("His a Student?: " + score + "/Type: " + typeof score);
+
+}
+
+var age = "Жасы: " 
+var age = "17"
+console.log("Жасы: " + age + "/Type: " + typeof age);
+
+
+
+var age = "Жасы"
+var age = "2"
+var age = "5"
+console.log("Жасы: " + "2" + "5" + "/Type: " + typeof age);
+
